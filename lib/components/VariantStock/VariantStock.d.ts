88ffd44d0ac50789299902585b67655ee2b7576e
Jarrett Retz <jarrett.retz@comcast.net>
2020-06-14 13:39:16 -0700

    build

--- lib/components/VariantStock/VariantStock.d.ts
+++ lib/components/VariantStock/VariantStock.d.ts
@@ -3,6 +3,7 @@ import { SnipcartVariation, Variation } from '../../util/getAvailableVariants';
 export interface IVariantStockProps {
     selected: Variation[];
     raw: SnipcartVariation[];
+    variationLength: Number;
 }
 declare const VariantStock: React.FC<IVariantStockProps>;
 export default VariantStock;
